c0b5b20ca33d077122c05ed07356324c9a7a0940
Jeremy Bi <bixuanxbi@gmail.com>
2015-08-15 20:54:04 +0800

    Add stack auto-complete

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 export MANPATH="/usr/local/man:/usr/man:/usr/local/share/man:/usr/share/man:/usr/local/pkg/perl/man:/usr/dt/man:/usr/openwin/man:/usr/sfw/man:/Users/jeremybi/local/man:/Users/jeremybi/local/share/man:/Users/jeremybi/.cabal/share/man:/usr/local/opt/coreutils/libexec/gnuman"
 
-export PATH="/Users/jeremybi/bin:/usr/local/share/npm/bin:/usr/X11R6/bin:/Users/jeremybi/.cask/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+export PATH="/usr/local/share/npm/bin:/usr/X11R6/bin:bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
 
 alias ec='emacsclient -c -n'
 alias ems='emacs --daemon'
@@ -33,7 +33,7 @@ fi
 # Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
 export GHC_DOT_APP="/Applications/ghc-7.10.2.app"
 if [ -d "$GHC_DOT_APP" ]; then
-    export PATH="${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
+    export PATH="${HOME}/bin:${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
 fi
 
 if [ -e /Users/jeremybi/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/jeremybi/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
@@ -42,3 +42,7 @@ export NIX_PATH=nixpkgs=/Users/jeremybi/Projects/nixpkgs
 
 alias rm='trash'
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
+
+export PATH="${HOME}/bin:${PATH}"
+
+eval "$(stack --bash-completion-script "$(which stack)")"
