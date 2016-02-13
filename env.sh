146b3a38d4d7f75ff14b96f39b3ad337b2c3825a
Jeremy Bi <bixuanxbi@gmail.com>
2016-02-13 14:02:23 +0800

    Update

--- env.sh
+++ env.sh
@@ -1,11 +1,8 @@
 #!/bin/zsh
 
-export MANPATH="/usr/local/man:/usr/man:/usr/local/share/man:/usr/share/man:/usr/local/pkg/perl/man:/usr/dt/man:/usr/openwin/man:/usr/sfw/man:/Users/jeremybi/local/man:/Users/jeremybi/local/share/man:/Users/jeremybi/.cabal/share/man:/usr/local/opt/coreutils/libexec/gnuman"
+export MANPATH="/usr/local/man:/usr/man:/usr/local/share/man:/usr/share/man:/usr/local/opt/coreutils/libexec/gnuman"
 
-export PATH="/usr/local/share/npm/bin:/usr/X11R6/bin:bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
-
-alias ec='emacsclient -c -n'
-alias ems='emacs --daemon'
+export PATH="/usr/local/bin:/usr/X11R6/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
 
 export JAVA_HOME=$(/usr/libexec/java_home)
 
@@ -30,12 +27,6 @@ if (( $+commands[lesspipe.sh] )); then
     export LESSOPEN='| /usr/bin/env lesspipe.sh %s 2>&-'
 fi
 
-# Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
-# export GHC_DOT_APP="/Applications/ghc-7.10.2.app"
-# if [ -d "$GHC_DOT_APP" ]; then
-#     export PATH="${HOME}/bin:${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
-# fi
-
 # if [ -e /Users/jeremybi/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/jeremybi/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
 
 # export NIX_PATH=nixpkgs=/Users/jeremybi/Projects/nixpkgs
@@ -43,8 +34,10 @@ fi
 alias rm='trash'
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
 
-export PATH="${HOME}/bin:${PATH}"
+export PATH="${HOME}/.local/bin:${PATH}"
 
 eval "$(stack --bash-completion-script "$(which stack)")"
 
 source <(f2j --bash-completion-script `which f2j`)
+
+alias emc="/usr/local/Cellar/emacs-mac/emacs-24.5-z-mac-5.13/Emacs.app/Contents/MacOS/Emacs &"
