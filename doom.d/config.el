b1a89ba2a9fec429877913ed91b6ff3af05f9774
Jeremy Bi <bixuanxbi@gmail.com>
2020-10-10 11:31:05 +0800

    use fourmolu

--- doom.d/config.el
+++ doom.d/config.el
@@ -52,3 +52,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! lsp-haskell
+  (setq lsp-haskell-diagnostics-on-change nil
+        lsp-haskell-formatting-provider "fourmolu"))
